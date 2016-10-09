ae8426cfaf0f001b0396ac8293cdafcf359d37aa
Jk Jensen <azjkjensen@gmail.com>
2016-10-09 16:21:42 -0600

    Implemented get and delete"

--- list/unrolled_linked_list/module.py
+++ list/unrolled_linked_list/module.py
@@ -1,3 +1,5 @@
+import math
+
 class Node():
     '''This '''
     def __init__(self):
@@ -11,6 +13,7 @@ class UnrolledLinkedList():
     """
     def __init__(self, max_node_capacity=16):
         self.max_node_capacity = max_node_capacity
+        self.length = 0
         self.head = None
 
     '''Remove the item at the given index.
@@ -18,12 +21,66 @@ class UnrolledLinkedList():
     (i.e. deleting at -2 would delete the second-to-last element)
     If the index is too large, raise an IndexError'''
     def __delItem__(self,index):
+        if index < 0:
+            absIndex = self.length + index
+        else:
+            absIndex = index
+        
+        if index > self.length - 1: # Over the max
+            raise IndexError(str(index) + ' out of range.')
+        elif absIndex < 0: # Below 0
+            raise IndexError(str(index) + 'out of range.')
+
+        # We now have a valid index.
+        currentNode = self.head
+        # Iterate until the right node is found.
+        while len(currentNode.arr) + currentIndex < absIndex:
+            currentIndex = currentIndex + len(currentNode.arr)
+            currentNode = currentNode.next
+
+        # We have the right node, time to get the item from the array.
+        arrIndex = absIndex - currentIndex 
+        del currentNode.arr[arrIndex] # Delete the data at the final index
+
+        # Rebalance the list if the current node's array 
+        # fell below half of max_node_capacity.
+        nextNode = currentNode.next
+        if len(currentNode.arr) < math.floor(self.max_node_capacity/2) and nextNode is not None:
+            if len(nextNode.arr) <= math.floor(self.max_node_capacity/2)+1:
+                # Merge the nodes
+                currentNode.arr = currentNode.arr + nextNode.arr
+                currentNode.next = nextNode.next
+                del nextNode
+            else:
+                # Pull an item from the next node
+                currentNode.arr.append(nextNode.arr[0])
+                del nextNode.arr[0]
+
+        
 
     """Returns the item in the given index.
     If the index is negative, return with the index starting from the back 
     (i.e. getting at -1 returns the last item)
     If the index is too large, raise an IndexError"""
     def __getitem__ (self,index):
+        if index < 0:
+            absIndex = self.length + index
+        else:
+            absIndex = index
+        
+        if index > self.length - 1: # Over the max
+            raise IndexError(str(index) + ' out of range.')
+        elif absIndex < 0: # Below 0
+            raise IndexError(str(index) + 'out of range.')
+        
+        # Iterate until the right node is found.
+        while len(currentNode.arr) + currentIndex < absIndex:
+            currentIndex = currentIndex + len(currentNode.arr)
+            currentNode = currentNode.next
+
+        # We have the right node, time to get the item from the array.
+        arrIndex = absIndex - currentIndex 
+        return currentNode.arr[arrIndex] # Delete the data at the final index
 
     '''Sets the item at key to value
     If the key is too large, raise an IndexError'''
